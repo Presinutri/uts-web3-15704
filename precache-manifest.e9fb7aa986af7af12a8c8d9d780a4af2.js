bd0085b059cb91d0dd20328e4094aa35aa420d99
farizalbab <farizalbab@gmail.com>
2020-04-15 15:22:11 +0700

    Updates

---
+++ precache-manifest.e9fb7aa986af7af12a8c8d9d780a4af2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5700666428db22384b51a6d1e647508e",
+    "url": "/uts-web3-15704/index.html"
+  },
+  {
+    "revision": "8f440164f583770c366d",
+    "url": "/uts-web3-15704/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "6dd2accce0f38089ed1b",
+    "url": "/uts-web3-15704/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "8f440164f583770c366d",
+    "url": "/uts-web3-15704/static/js/2.af11a80f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/uts-web3-15704/static/js/2.af11a80f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6dd2accce0f38089ed1b",
+    "url": "/uts-web3-15704/static/js/main.532c4baf.chunk.js"
+  },
+  {
+    "revision": "eb48a39e2d83c43ce0e9",
+    "url": "/uts-web3-15704/static/js/runtime-main.01657b3c.js"
+  }
+]);
