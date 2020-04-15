93d774f1ade6f3a1db090d9f913e559f177bb642
farizalbab <farizalbab@gmail.com>
2020-04-15 15:21:13 +0700

    deploy nih

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import React from 'react';
+import './App.css';
 import {
   BrowserRouter,
   Switch,
+  Route,
 } 
 from "react-router-dom";
 import Header from "./Header";
 import Blog from "./Blog";
+import BlogDetail from "./BlogDetail";
+
 
 //install bootstrap dulu
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -17,19 +22,15 @@ function App() {
       <div className="App">
         <Header />
         <Switch>
-          <Route path="/about">
-            About
-          </Route>
-          <Route path="/blog/:articleId" component=BlogDetail />
-          <Route path="/blog">
-            <Blog />
-          </Route>
-          <Route path="/">
-            Home
-          </Route>
+          <Route path="/about">About</Route>
+          <Route path="/blog/:articleId" component={BlogDetail} />
+          <Route path="/blog" component={Blog}><Blog /></Route>
+          <Route path="/">Home</Route>
         </Switch>
       </div>
     </BrowserRouter>
   );
 }
 
+
+export default App;
